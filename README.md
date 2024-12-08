242ad5bb953daf143df473de06b09b127f691307
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2024-12-08 17:59:18 +0600

    readme.md file added

---
+++ README.md
@@ -0,0 +1,20 @@
+# Website Name: FIELD 
+
+# Feature: 
+
+--Interactive UI with modern Tools
+
+--User Authentication and Personalized Content
+
+--Product Details with high-Quality Images
+
+--Responsive and Mobile friendly Design
+
+--Real Time Data Handling and Update
+
+--Customizable Product Display with Filter
+
+
+# Live site link:"https://finicky-camp.surge.sh/"
+
+
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 const app = express()
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const equipmentCollection = client.db('equipmentDB').collection('equipment')
 
@@ -102,7 +102,7 @@ async function run() {
   
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
